1836bcf07bd4f5160308e5f840c29e9023a97de9
code-and-technology <tony.gottschalk@alumni.unc.edu>
2019-10-08 07:49:36 -0400

    begin scaffold of app

---
+++ app.js
@@ -0,0 +1,12 @@
+// backbone-youtube
+
+// move to config or dotenv
+const apiKey = {
+	search: "&key=AIzaSyB3YECTEhQkCY9CyYu43DQGgjP9RL2UIHQ"
+};
+
+const appModel = new AppModel();
+const appView = new AppView({
+  model: appModel
+});
+
